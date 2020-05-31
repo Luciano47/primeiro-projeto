5f7d7416764a7eb467d1c0047f98232334a1dfb7
Luciano47 <61392147+Luciano47@users.noreply.github.com>
2020-05-31 12:12:56 -0300

    Adicionei arquivos via upload

---
+++ Hello githanb/src/com/br/lucianoalves/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.br.lucianoalves.git;
+
+/**
+ * @author Luciano
+ *exemplo e innicio de pragrama java eclipse
+ */
+public class HelloGit {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("AMOR FEINHOEu quero amor feinho.Amor feinho não olha um pro outro.Uma vez encontrado é igual fé,não teologa mais.");
+
+	}
+
+}
